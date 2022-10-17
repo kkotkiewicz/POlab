8d78e0b04428341a2e53c055e32e08dcb97b0031
kkotkiewicz <kapeczek32@gmail.com>
2022-10-17 17:53:38 +0200

    Zmiany do lab2

---
+++ src/test/java/agh/ics/oop/MapDirectionTest.java
@@ -0,0 +1,16 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapDirectionTest {
+
+    @Test
+    void next() {
+    }
+
+    @Test
+    void previuos() {
+    }
+}
---
+++ src/test/java/agh/ics/oop/Vector2dTest.java
@@ -0,0 +1,44 @@
+package agh.ics.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Vector2dTest {
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void precedes() {
+    }
+
+    @Test
+    void follows() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void upperRight() {
+    }
+
+    @Test
+    void lowerLeft() {
+    }
+
+    @Test
+    void opposite() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
